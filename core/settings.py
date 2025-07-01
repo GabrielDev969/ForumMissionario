93d1f08f5f4a625eee7da10a07c649a14e7328d9
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-01 15:16:57 -0300

    :wrench: Adjusted settings sqlite

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,10 +67,10 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # Fallback for local dev
+        conn_max_age=600
+    )
 }
 
 
